26ef8bb5aae59c802d6f27a2162e10752fc86805
olivermharrison <olivermharrison@gmail.com>
2022-06-06 15:19:11 +0100

    docs: add swagger docs

--- handler/accounts.go
+++ handler/accounts.go
@@ -2,10 +2,16 @@ package handler
 
 import (
 	"fmt"
+	"kidsloop/account-service/model"
 
 	"github.com/gin-gonic/gin"
 )
 
+// CreateAccount ... Create Account
+// @Summary  Create a new account
+// @Success  200      {object}  model.Account
+// @Failure  400,500  {object}  model.APIError
+// @Router   /accounts [put]
 func CreateAccount(c *gin.Context) {
 	fmt.Println("CreateAccount")
 	c.JSON(200, gin.H{
@@ -13,14 +19,27 @@ func CreateAccount(c *gin.Context) {
 	})
 }
 
+// GetAccount ... Get Account
+// @Summary  Get details of account
+// @Param    accountId  path      string  true  "Account ID"
+// @Success  200        {object}  model.Account
+// @Failure  400,500    {object}  model.APIError
+// @Router   /accounts/{accountId} [get]
 func GetAccount(c *gin.Context) {
 	fmt.Println("GetAccount")
 	accountId := c.Param("accountId")
-	c.JSON(200, gin.H{
-		"accountId": accountId,
-	})
+	account := model.Account{
+		ID: accountId,
+	}
+	c.JSON(200, account)
 }
 
+// DeleteAccount ... Delete Account
+// @Summary  Delete an account
+// @Param    accountId  query     string  true  "Account ID"
+// @Success  200        {object}  model.Account
+// @Failure  400,500    {object}  model.APIError
+// @Router   /accounts/{accountId} [delete]
 func DeleteAccount(c *gin.Context) {
 	fmt.Println("DeleteAccount")
 	accountId := c.Param("accountId")
--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	_ "kidsloop/account-service/docs"
 	"kidsloop/account-service/handler"
 
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"   // gin-swagger middleware
+	"github.com/swaggo/gin-swagger/swaggerFiles" // swagger embed files
 )
+
+// @title    accounts-service documentation
+// @version  1.0.0
+// @host     localhost:8080
 
 func main() {
 	fmt.Println("Starting KidsLoop Account Service on http://localhost:8080")
@@ -21,6 +28,8 @@ func SetUpRouter() *gin.Engine {
 	r.GET("/accounts/:accountId", handler.GetAccount)
 	r.PUT("/accounts", handler.CreateAccount)
 	r.DELETE("/accounts/:accountId", handler.DeleteAccount)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	return r
 }
 

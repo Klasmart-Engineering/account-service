3b7edd7d4d801dfcf33b75ec28170bb783d25ffd
Matt E <matthew.entwistle@kidsloop.live>
2022-06-27 16:45:43 +0100

    feat(AS-32): get android endpoint

---
+++ handler/androids.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"context"
+	db "kidsloop/account-service/database"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/v3/integrations/nrgin"
+	"github.com/newrelic/go-agent/v3/newrelic"
+)
+
+// GetAndroid ... Get Android
+// @Summary  Get details of an android
+// @Tags     androids
+// @Param    id           path      string  true  "Account ID"
+// @Success  200          {object}  model.Android
+// @Failure  400,404,500  {object}  model.ErrorResponse
+// @Router   /androids/{id} [get]
+func GetAndroid(c *gin.Context) {
+	type Uri struct {
+		ID string `uri:"id" binding:"required,uuid"`
+	}
+	var uri Uri
+	if err := c.ShouldBindUri(&uri); err != nil {
+		c.Error(err)
+		return
+	}
+
+	nrTxn := nrgin.Transaction(c)
+	nrCtx := newrelic.NewContext(context.Background(), nrTxn)
+
+	id := c.Param("id")
+	android, err := db.Database.GetAndroid(nil, nrCtx, id)
+
+	if err != nil {
+		c.Error(err)
+	} else {
+		c.JSON(http.StatusOK, android)
+	}
+}
